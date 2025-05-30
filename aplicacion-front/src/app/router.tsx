158ab469560d44f06067481a384c1391414be948
Dafonseca15 <da.fonseca@uniandes.edu.co>
2025-05-30 00:51:06 -0500

    Field architecture

---
+++ aplicacion-front/src/app/router.tsx
@@ -0,0 +1,8 @@
+export const AppRouter = () => {
+  return (
+    <div className="app-router">
+      <h1>Welcome to the App Router</h1>
+      <p>This is where your routing logic will go.</p>
+    </div>
+  );
+}
