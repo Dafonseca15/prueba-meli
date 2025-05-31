48fd4258f03e3d2102c0bee9e521e9400582b823
Dafonseca15 <da.fonseca@uniandes.edu.co>
2025-05-31 16:10:49 -0500

    Craacion de utilidades a nivel global y componente
    Se crearon utilidades para ser reusables de manera global y a nivel componente.
    - Global: formatPrice()
    - Componente: calculateDiscount(), discountPercentage()

---
+++ aplicacion-front/src/features/product-detail/utils/utils.ts
@@ -0,0 +1,14 @@
+const calculateDiscount = (original: number, current: number) => {
+    if (!original || original <= current) return null;
+    return Math.round(((original - current) / original) * 100);
+};
+
+const discountPercentage = (original: number, current: number) => {
+    const discount = calculateDiscount(original, current);
+    return discount !== null ? `${discount}%` : '';
+}
+
+export { 
+    calculateDiscount,
+    discountPercentage
+};
